521459c8dba6acae05c0e6a5bcdd4c6484700d96
Enrique Coscarelli <enriquecoscarelli@gmail.com>
2021-06-07 17:32:04 +0200

    Checked for functionality altering app.js

--- src/App.js
+++ src/App.js
@@ -6,16 +6,21 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        
+        
+        <p>Hello World</p>
+        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edzxczxczxczxit <code>src/App.js</code> and save to reload.
         </p>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          World
         </a>
       </header>
     </div>
