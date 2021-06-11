0b35507490eba5f957caa5f3947a5776d6e9a94e
Enrique Coscarelli <enriquecoscarelli@gmail.com>
2021-06-11 14:22:22 +0200

    Changed styles of navbar and footer in Adobe XD file

---
+++ src/context/auth-context.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import authService from './../lib/auth-service';
+
+const { Consumer, Provider } = React.createContext();
+
+
+class AuthProvider extends React.Component {
+  state = {
+    isLoggedIn: false,
+    isLoading: true,
+    user: null
+  }
+
+  componentDidMount () {
+    authService.me()
+     .then((user) => this.setState({ isLoggedIn: true, user: user, isLoading: false }))
+     .catch((err) => this.setState({ isLoggedIn: false, user: null, isLoading: false }));
+  }
+
+  signup = (username, password) => {
+    authService.signup( username, password )
+      .then((user) => this.setState({ isLoggedIn: true, user }) )
+      .catch((err) => {
+        this.setState({ isLoggedIn: false, user: null });
+      })
+  }
+
+  login = (username, password) => {
+    authService.login( username, password )
+      .then((user) => this.setState({ isLoggedIn: true, user }))
+      .catch((err) => {
+        this.setState({ isLoggedIn: false, user: null });
+      })
+  }
+
+  logout = () => {
+    authService.logout()
+      .then(() => this.setState({ isLoggedIn: false, user: null }))
+      .catch((err) => console.log(err));
+  }
+  me = () => {
+    authService
+      .me()
+      .then((user) => {
+        this.setState({ user });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  render() {
+    const { isLoggedIn, isLoading, user } = this.state;
+    const { signup, login, logout, me } = this;
+    if (isLoading) return <p>Loading</p>;
+    return(
+      <Provider value={{ isLoggedIn, isLoading, user, signup, login, logout, me }}  >
+        {this.props.children}
+      </Provider>
+    )
+  }
+}
+// HOC that converts regular component into a Consumer
+const withAuth = (WrappedComponent) => {
+  return class extends React.Component {
+    render() {
+      return(
+        <Consumer>
+          { (value) => {
+            const { isLoggedIn, isLoading, user, signup, login, logout, me } = value;
+            return (<WrappedComponent 
+                      {...this.props}
+                      isLoggedIn={isLoggedIn} 
+                      isLoading={isLoading} 
+                      user={user} 
+                      signup={signup} 
+                      login={login} 
+                      logout={logout}
+                      me={me}
+                    />)
+          } }
+        </Consumer>
+        )
+    }
+}
+}
+export { AuthProvider, withAuth }
