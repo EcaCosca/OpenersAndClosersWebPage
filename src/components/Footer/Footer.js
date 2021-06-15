8a33d3bd4ef3f09c73829a7a5776d21077bae5aa
Enrique Coscarelli <enriquecoscarelli@gmail.com>
2021-06-15 14:45:48 +0200

    First commit on footer files development

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {Nav, NavLink, Bars, NavMenu, NavBtn, NavBtnLink} from './FooterElements';
+
+
+const Navbar = () => {
+    return (
+        <>
+            <Nav>
+                <NavLink to="/">
+                    <img src={require('../../images/companyLogo.png')} alt="Openers and Closers Logo"/>
+                </NavLink>
+                <Bars />
+                <NavMenu>
+                    <NavLink to="/electronicos" activeStyle>
+                        CERRADEROS ELECTRÓNICOS
+                    </NavLink>
+                    <NavLink to="/electricos" activeStyle>
+                        CERRADEROS ELÉCTRICOS
+                    </NavLink>
+                    <NavLink to="/electromagneticos" activeStyle>
+                        CERRADURAS ELECTROMAGNÉTICOS
+                    </NavLink>
+                    <NavLink to="/seguridad" activeStyle>
+                        CERRADURAS DE SEGURIDAD
+                    </NavLink>
+                    <NavLink to="/accesorios" activeStyle>
+                        ACCESORIOS
+                    </NavLink>
+                </NavMenu> 
+            </Nav>
+        </>
+    )
+};
+
+export default Navbar
