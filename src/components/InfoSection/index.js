472adcc4ead630aa5ff2186ca9bb0423f2427e56
Enrique Coscarelli <enriquecoscarelli@gmail.com>
2021-06-22 16:20:25 +0200

    added structure to the InfoContainer component and styles as well

--- src/components/InfoSection/index.js
+++ src/components/InfoSection/index.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import { InfoContainer, InfoWrapper, InfoRow, Column1, Column2, TextWrapper, TopLine, Heading, Subtitle, ImgWrap, Img, BtnWrap } from './InfoElements'
 
 const InfoSection = () => {
     return (
         <>
-            
+            <InfoContainer id={id}>
+                <InfoWrapper>
+                    <InfoRow>
+                        <Column1>
+                            <TextWrapper>
+                                <TopLine>TopLine</TopLine>
+                                <Heading>Heading</Heading>
+                                <Subtitle>Subtitle</Subtitle>
+                                <BtnWrap>
+                                    <Button to='home'>
+                                        Button
+                                    </Button>
+                                </BtnWrap>
+                            </TextWrapper>
+                        </Column1>
+                        <Column2>
+                            <ImgWrap>
+                                <Img />
+                            </ImgWrap>
+                        </Column2>
+                    </InfoRow>
+                </InfoWrapper>
+            </InfoContainer>
         </>
     )
 }
