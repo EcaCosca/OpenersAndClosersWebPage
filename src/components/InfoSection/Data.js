c62eade0b1a5a3f22fd0866b8106c0662bbef38b
Enrique Coscarelli <enriquecoscarelli@gmail.com>
2021-06-23 17:30:20 +0200

    created services component

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -7,9 +7,42 @@ export const homeObjOne = {
     description: 'Inaugurating a new factory has allowed us to consolidate. The new headquarters has been designed taking into account the comfort and practicality of spaces for daily use: large windows ensure the entry of natural light, open spaces allow the ease of information flow and jobs are oriented towards the digitalization',
     buttonLabel: 'Get started',
     imgStart: false,
-    img: require('../../images/undraw_map_1r69.png'),
+    img: require('../../images/svg1.svg'),
     alt: 'Car',
     dark: false,
     primary: false,
     darkText: true
 }
+
+export const homeObjTwo = {
+    id: 'discover',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    headline: 'Lorem Ipsum',
+    description: 'Inaugurating a new factory has allowed us to consolidate. The new headquarters has been designed taking into account the comfort and practicality of spaces for daily use: large windows ensure the entry of natural light, open spaces allow the ease of information flow and jobs are oriented towards the digitalization',
+    buttonLabel: 'Get started',
+    imgStart: true,
+    img: require('../../images/svg1.svg'),
+    alt: 'amng',
+    dark: false,
+    primary: false,
+    darkText: true
+}
+
+export const homeObjThree = {
+    id: 'about',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    headline: 'New corporate headquarters',
+    description: 'Inaugurating a new factory has allowed us to consolidate. The new headquarters has been designed taking into account the comfort and practicality of spaces for daily use: large windows ensure the entry of natural light, open spaces allow the ease of information flow and jobs are oriented towards the digitalization',
+    buttonLabel: 'Get started',
+    imgStart: false,
+    img: require('../../images/svg1.svg'),
+    alt: 'Car',
+    dark: false,
+    primary: false,
+    darkText: true
+}
+
---
+++ src/components/Services/Services.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Services = () => {
+    return (
+        <ServicesContainer>
+            <ServicesH1>Our Services</ServicesH1>
+            <ServicesWrapper>
+                <ServicesCard>
+                    <ServicesIcon src={Icon1}/>
+
+                    <ServicesH1>Our Services</ServicesH1>
+                    <ServicesP>Secure locking solutions</ServicesP>
+                </ServicesCard>
+                <ServicesCard>
+                    <ServicesIcon src={Icon2}/>
+
+                    <ServicesH1>Our Services</ServicesH1>
+                    <ServicesP>Ssld ibgaj gbò aihgo n io oih piugo ufoiytc ytxd iytxcl ucop cñi gñi jgñyi cv.s</ServicesP>
+                </ServicesCard>
+                <ServicesCard>
+                    <ServicesIcon src={Icon3}/>
+
+                    <ServicesH1>Our Services</ServicesH1>
+                    <ServicesP> iub piug uigñu igñi fo fuy diyk cvuñgñ uyc lyx tdflñi ñugt dñu -cl tdf.k</ServicesP>
+                </ServicesCard>
+            </ServicesWrapper>
+        </ServicesContainer>
+    )
+}
+
+export default Services
