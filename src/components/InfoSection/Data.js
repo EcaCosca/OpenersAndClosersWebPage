2d1770ee5c99dd07126053bd8080d8a0b6edf9c7
Enrique Coscarelli <enriquecoscarelli@gmail.com>
2021-06-23 11:30:46 +0200

    Added al information on to data.js

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,15 @@
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    headline: 'New corporate headquarters',
+    description: 'Inaugurating a new factory has allowed us to consolidate. The new headquarters has been designed taking into account the comfort and practicality of spaces for daily use: large windows ensure the entry of natural light, open spaces allow the ease of information flow and jobs are oriented towards the digitalization',
+    buttonLabel: 'Get started',
+    imgStart: false,
+    img: require('../../images/companyLogo.png'),
+    alt: 'Car',
+    dark: true,
+    primary: true,
+    darkText: false
+}
