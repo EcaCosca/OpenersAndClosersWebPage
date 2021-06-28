6b1d78d3200243135fd82acc76d0cd721800197f
Enrique Coscarelli <enriquecoscarelli@gmail.com>
2021-06-28 11:48:31 +0200

    Changed the information in footer section, refactored the code and added routes to PRODUCTOS section

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -9,54 +9,44 @@ function Footer() {
           <FooterLinkWrapper>
           {/* Column 1 */}
             <FooterLinkItems>
-              <FooterLinkTitle>This is the title</FooterLinkTitle>
-                <FooterLink to="/">Yeah!</FooterLink>
-                <FooterLink to="/">Yeah!</FooterLink>
-                <FooterLink to="/">Yeah!</FooterLink>
-                <FooterLink to="/">Yeah!</FooterLink>
-                <FooterLink to="/">Yeah!</FooterLink>
-                <FooterLink to="/">Yeah!</FooterLink>
-                <FooterLink to="/">Yeah!</FooterLink>
+              <FooterLinkTitle>SOBRE NOSOTROS</FooterLinkTitle>
+                <FooterLink to="/">Nuestra historia</FooterLink>
+                <FooterLink to="/">Mision, valores y vision</FooterLink>
+                <FooterLink to="/">Memoria corporativa</FooterLink>
+                <FooterLink to="/">Responsabilidad social</FooterLink>
             </FooterLinkItems>
           {/* Column 2 */}
             <FooterLinkItems>
-              <FooterLinkTitle>This is the title</FooterLinkTitle>
-                <FooterLink to="/">Yeah!</FooterLink>
-                <FooterLink to="/">Yeah!</FooterLink>
-                <FooterLink to="/">Yeah!</FooterLink>
-                <FooterLink to="/">Yeah!</FooterLink>
-                <FooterLink to="/">Yeah!</FooterLink>
-                <FooterLink to="/">Yeah!</FooterLink>
-                <FooterLink to="/">Yeah!</FooterLink>
+              <FooterLinkTitle>PRODUCTOS</FooterLinkTitle>
+                <FooterLink to="/electronicos">Cerraderos electronicos</FooterLink>
+                <FooterLink to="/electricos">Cerraderos eléctricos</FooterLink>
+                <FooterLink to="/electromagneticas">Cerraduras electromagnéticas</FooterLink>
+                <FooterLink to="/seguridad">Cerraduras de seguridad</FooterLink>
+                <FooterLink to="/accesorios">Accesorios</FooterLink>
             </FooterLinkItems>
           </FooterLinkWrapper>
           <FooterLinkWrapper>
           {/* Column 3 */}
             <FooterLinkItems>
-              <FooterLinkTitle>This is the title</FooterLinkTitle>
-                <FooterLink to="/">Yeah!</FooterLink>
-                <FooterLink to="/">Yeah!</FooterLink>
-                <FooterLink to="/">Yeah!</FooterLink>
-                <FooterLink to="/">Yeah!</FooterLink>
-                <FooterLink to="/">Yeah!</FooterLink>
-                <FooterLink to="/">Yeah!</FooterLink>
-                <FooterLink to="/">Yeah!</FooterLink>
+              <FooterLinkTitle>SERVICIOS</FooterLinkTitle>
+                <FooterLink to="/">Consultoria tecnologica</FooterLink>
+                <FooterLink to="/">Atencion al cliente</FooterLink>
+                <FooterLink to="/">Formacion de producto</FooterLink>
+                <FooterLink to="/">Calendario de eventos</FooterLink>
+                <FooterLink to="/">Descargas</FooterLink>
             </FooterLinkItems>
           {/* Column 4 */}
             <FooterLinkItems>
-              <FooterLinkTitle>This is the title</FooterLinkTitle>
-                <FooterLink to="/">Yeah!</FooterLink>
-                <FooterLink to="/">Yeah!</FooterLink>
-                <FooterLink to="/">Yeah!</FooterLink>
-                <FooterLink to="/">Yeah!</FooterLink>
-                <FooterLink to="/">Yeah!</FooterLink>
-                <FooterLink to="/">Yeah!</FooterLink>
-                <FooterLink to="/">Yeah!</FooterLink>
+              {/* <FooterLinkTitle>This is the title</FooterLinkTitle> */}
+                <FooterLink to="/">Soluciones</FooterLink>
+                <FooterLink to="/">Configurador</FooterLink>
+                <FooterLink to="/">Tienda</FooterLink>
+                <FooterLink to="/">Contacto</FooterLink>
             </FooterLinkItems>
           </FooterLinkWrapper>
           <FooterLinkWrapper>
           {/* Column 5 */}
-            <FooterLinkItems>
+            {/* <FooterLinkItems>
               <FooterLinkTitle>This is the title</FooterLinkTitle>
                 <FooterLink to="/">Yeah!</FooterLink>
                 <FooterLink to="/">Yeah!</FooterLink>
@@ -65,7 +55,7 @@ function Footer() {
                 <FooterLink to="/">Yeah!</FooterLink>
                 <FooterLink to="/">Yeah!</FooterLink>
                 <FooterLink to="/">Yeah!</FooterLink>
-            </FooterLinkItems>
+            </FooterLinkItems> */}
           </FooterLinkWrapper>
         </FooterLinksContainer>
       </FooterWrap>
