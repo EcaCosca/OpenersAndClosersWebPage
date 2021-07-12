513c43e48aad31b6b37baa4c6d40d23df2806c35
Enrique Coscarelli <enriquecoscarelli@gmail.com>
2021-07-12 17:27:44 +0200

    installed npm package for axios hooks testing on create user

--- src/components/Createuser/CreateUser.js
+++ src/components/Createuser/CreateUser.js
@@ -1,18 +1,53 @@
 import axios from 'axios'
+import useAxios from 'axios-hooks'
 import React, { Component } from 'react'
 
-export default class CreateUser extends Component {
+function CreateUser() {
 
-    async componentDidMount() {
-        const res = await axios.get('http://localhost:4000/api/users');
-        console.log(res);
-    } 
+    const [{ data, loading, error }, refetch] = useAxios(
+        'http://localhost:4000/api/users'
+      )
+    
+      if (loading) return <p>Loading...</p>
+      if (error) return <p>Error!</p>
+    
+    console.log(data);
+    console.log(loading);
+    console.log(error);
+    
+    // async componentDidMount() {
+    //     const res = await axios.get('http://localhost:4000/api/users');
+    //     this.setState({users: res.data});
+    //     console.log(this.state.users);
+    //     // this.getUsers();
+    // } 
 
-    render() {
+    
         return (
-            <div>
-                CreateUser
+            
+
+            
+            <div className="row">
+                <div className="col-md-4">
+                    form user
+                </div>
+                <div className="col-md-8">
+                    <ul className="list-group">
+                        <li>
+                            {/* {JSON.stringify(data.email)} */}
+                        </li>
+                        
+                        {
+                            {/* this.state.user.map(user => <li>
+                                {user.email}
+                            </li>) */}
+                        }
+                    </ul>
+                </div>
             </div>
         )
-    }
+    
 }
+
+
+export default CreateUser
