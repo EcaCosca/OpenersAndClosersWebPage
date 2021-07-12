5fc17adb9bd7a26321af8cdf8f00b6553aced59b
Enrique Coscarelli <enriquecoscarelli@gmail.com>
2021-07-12 10:32:25 +0200

    refactored some code, cleaned a few bugs and changed the axios request to port 4000

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,7 +1,17 @@
 import React, {useState} from 'react'
 import Video from '../videos/videolqshort.mp4'
-import { Button } from '../ButtonElements';
-import { HeroContainer, HeroBg, VideoBg, HeroContent, HeroH1, HeroP, HeroBtnWrapper, ArrowForward, ArrowRight } from './HeroElements'
+// import { Button } from '../ButtonElements';
+import { 
+    HeroContainer,
+    HeroBg,
+    VideoBg,
+    HeroContent,
+    HeroH1,
+    HeroP,
+    HeroBtnWrapper,
+    // ArrowForward,
+    // ArrowRight 
+} from './HeroElements'
 
 const HeroSection = () => {
     const [hover, setHover] = useState(false)
