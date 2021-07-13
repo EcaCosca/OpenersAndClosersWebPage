0fba05ff04a1b6ebf7d3014bb42eff54fbd60a97
Enrique Coscarelli <enriquecoscarelli@gmail.com>
2021-07-13 17:24:42 +0200

    just changed the method from useAxios hook to axio with useEffect to map the array of users

--- src/components/Createuser/CreateUser.js
+++ src/components/Createuser/CreateUser.js
@@ -1,31 +1,61 @@
+// import useAxios from 'axios-hooks'
 import axios from 'axios'
-import useAxios from 'axios-hooks'
 import React, { useState, useEffect } from 'react'
 
 function CreateUser() {
+    const [data, getData] = useState( [] );
 
-    const [{ data, loading, error, response }, refetch] = useAxios({
-        url: 'http://localhost:4000/api/users',
-        method: 'GET',
-      });
-      const [text, setText] = useState('');
+    useEffect(() => (
+        axios
+        .get('http://localhost:4000/api/users')
+        .then(result => getData(result.data))
+
+    ), [])
+
+    // USING AXIOS HOOK useAxios
+
+    // const [{ data, loading, error, response }, refetch] = useAxios({
+    //     url: 'http://localhost:4000/api/users',
+    //     method: 'GET',
+    //   });
+    //   const [text, setText] = useState('');
 
 
-      useEffect(() => {
-          if (data) {
-              setText(data.email);
-          }else if (error) {
-              setText=(error.message);
-          }
-      }, [data, error]);
+    //   useEffect(() => {
+    //       if (data) {
+    //           setText(data.email);
+    //       }else if (error) {
+    //           setText=(error.message);
+    //       }
+    //   }, [data, error]);
 
-      console.table(data);
+    //   console.log(data);
+
+    //   console.table(data);
+
+    //   const allUsersArray = data;
+
+    //   console.log(allUsersArray);
+
     
         return (
             
 
             <div>
                 <h1>here</h1>
+                {/* <h2>{allUsersArray}</h2> */}
+                
+                <ul>
+                    {
+                        data.map(users => (
+                            <li>
+                                {users.email}
+                            </li>
+                        ))
+                    }
+                </ul>
+
+                {/* <p>{allUsersArray}</p> */}
 
                 <div className="row">
                     <div className="col-md-4">
@@ -34,7 +64,8 @@ function CreateUser() {
                     <div className="col-md-8">
                         <ul className="list-group">
                             <li>
-                                {JSON.stringify(data)}
+                                {/* {JSON.stringify(data)} */}
+                                {/* {data.email} */}
                             </li>
                         </ul>
                     </div>
