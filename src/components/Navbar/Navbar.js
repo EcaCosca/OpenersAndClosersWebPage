eb1e12e2e0eae11ba171eae7ca6298b1aa5a63e0
Enrique Coscarelli <enriquecoscarelli@gmail.com>
2021-07-15 17:57:41 +0200

    testing new button link

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -57,8 +57,9 @@ const Navbar = ({ toggle }) => {
                     </NavLink>
                 </NavMenu>
                 <NavBtn>
-                    {/* <NavBtnLink to="/signin">Sign Up</NavBtnLink> */}
-                    <NavBtnLink to="/create">Create User</NavBtnLink>
+                    <NavBtnLink to="/signin">Sign Up</NavBtnLink>
+                    {/* <NavBtnLink to="/create">Create User</NavBtnLink> */}
+                    {/* <NavBtnLink to="//openers-closers.es/source/CAT_ENERO%202020_WEB_ESP.pdf">Catalogo</NavBtnLink> */}
                 </NavBtn>
             </Nav>
         </IconContext.Provider>
