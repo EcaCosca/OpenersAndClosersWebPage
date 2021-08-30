2597f0aaff4a8a7bc94de7e3251dd81d17a95310
Enrique Coscarelli <enriquecoscarelli@gmail.com>
2021-08-30 17:27:59 +0200

    Testing animatedtxt package

--- src/components/Product/Header/ProductHeader.js
+++ src/components/Product/Header/ProductHeader.js
@@ -1,10 +1,26 @@
 import React from 'react'
+import { Phrase, Char } from "animatedtxt";
+import { ProductHeaderContainer, ProductH1 } from './ProductHeaderElements';
 
 const ProductHeader = () => {
+    
     return (
-        <div>
-            
-        </div>
+        <ProductHeaderContainer>
+            <ProductH1>
+                <Phrase margin={20} size={100} duration={1} color="#ffffff">
+                    <Char char="C" />
+                    <Char char="E" />
+                    <Char char="U" />
+                    <Char char="R" />
+                    <Char char="N" />
+                    <Char char="A" />
+                    <Char char="L" />
+                    <Char char="I" />
+                    <Char char="S" />
+                    <Char char="T" />
+                </Phrase>
+            </ProductH1>
+        </ProductHeaderContainer>
     )
 }
 
