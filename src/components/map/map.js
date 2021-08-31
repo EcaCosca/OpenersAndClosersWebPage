c2144824093583b6ce6562953d9e31aa5a1072b4
Enrique Coscarelli <enriquecoscarelli@gmail.com>
2021-08-31 15:25:50 +0200

    Map component displaying

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { Component } from 'react';
+import { Map, Marker, GoogleApiWrapper } from 'google-maps-react'
 
-const Map = () => {
+class MapApp extends Component {
+    render() {
+    const style = {
+     width: '300px',
+     height: '300px'
+     }
     return (
-        <div>
-            <h1>YO!</h1>
-        </div>
-    )
-}
-
-export default Map
+     <div className='container'>
+     Hello World
+     <Map 
+     google={this.props.google} 
+     zoom={10}
+     initialCenter={{
+     lat: 35.5496939,
+     lng: -120.7060049
+     }}
+     style={style}>
+     <Marker />
+     </Map>
+     </div>
+     );
+     }
+    }
+    export default GoogleApiWrapper({
+     apiKey: ('apikey'),
+     version: 3.31
+    })(MapApp);
