7766c06e31fa1bf4b8a4480a9fbc7c6bd764024a
Enrique Coscarelli <enriquecoscarelli@gmail.com>
2021-08-31 16:32:33 +0200

    Testing map method

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -1,30 +1,48 @@
-import React, { Component } from 'react';
-import { Map, Marker, GoogleApiWrapper } from 'google-maps-react'
+// import React, { Component } from 'react';
+// // import { Map, Marker, GoogleApiWrapper } from 'google-maps-react'
+// import { MapContainer } from './MapElements'
 
-class MapApp extends Component {
-    render() {
-    const style = {
-     width: '300px',
-     height: '300px'
-     }
+// class MapApp extends Component {
+//     render() {
+//     // const style = {
+//     //  width: '500px',
+//     //  height: '500px'
+//     //  }
+//     return (
+//      <MapContainer>
+//         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2993.1686216878397!2d2.0313099517885314!3d41.39214427916179!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12a49a5095397b7b%3A0xb062e251d7a117a5!2sCarrer%20Agricultura%2C%2017%2C%20Nave%2012%2C%2008980%20Sant%20Feliu%20de%20Llobregat%2C%20Barcelona!5e0!3m2!1ses!2ses!4v1630419665217!5m2!1ses!2ses" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+//         {/* <div className='container'>
+//             <Map 
+//             google={this.props.google} 
+//             zoom={10}
+//             initialCenter={{
+//             lat: 35.5496939,
+//             lng: -120.7060049
+//             }}
+//             style={style}>
+//                 <Marker />
+//             </Map>
+//         </div> */}
+//      </MapContainer>
+//      );
+
+//      }
+//     }
+//     // export default GoogleApiWrapper({
+//     //  apiKey: ('apikey'),
+//     //  version: 3.31
+//     // })(MapApp);
+
+//     export default MapApp
+
+import React from 'react'
+
+const MapApp = () => {
     return (
-     <div className='container'>
-     Hello World
-     <Map 
-     google={this.props.google} 
-     zoom={10}
-     initialCenter={{
-     lat: 35.5496939,
-     lng: -120.7060049
-     }}
-     style={style}>
-     <Marker />
-     </Map>
-     </div>
-     );
-     }
-    }
-    export default GoogleApiWrapper({
-     apiKey: ('apikey'),
-     version: 3.31
-    })(MapApp);
+        <div>
+            {/* <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2993.1686216878397!2d2.0313099517885314!3d41.39214427916179!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12a49a5095397b7b%3A0xb062e251d7a117a5!2sCarrer%20Agricultura%2C%2017%2C%20Nave%2012%2C%2008980%20Sant%20Feliu%20de%20Llobregat%2C%20Barcelona!5e0!3m2!1ses!2ses!4v1630419665217!5m2!1ses!2ses" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe> */}
+        </div>
+    )
+}
+
+export default MapApp
---
+++ src/components/map/mapElements.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const MapContainer = styled.div`
+    height: 800px;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-content: center;
+`
