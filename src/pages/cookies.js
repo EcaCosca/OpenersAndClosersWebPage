3da6f9eee971994b2edebeebc053e42a2979465a
Enrique Coscarelli <enriquecoscarelli@gmail.com>
2021-09-07 12:33:35 +0200

    eSCTRUCTURA EN POLITICA DE COOKIES

--- src/pages/cookies.js
+++ src/pages/cookies.js
@@ -1,9 +1,41 @@
 import React from 'react'
+import Navbar from '../components/Navbar/Navbar'
+import Footer from '../components/Footer'
+import { Phrase, Char } from "animatedtxt";
+import { ProductHeaderContainer, ProductH1 } from '../components/Product/Header/ProductHeaderElements';
 
 const cookies = () => {
     return (
         <div>
-            <h1>Cookies</h1>
+            <Navbar />
+            <ProductHeaderContainer>
+            <ProductH1>
+                <Phrase margin={20} size={60} duration={1} color="#ffffff">
+                    <Char char="P" />
+                    <Char char="O" />
+                    <Char char="L" />
+                    <Char char="I" />
+                    <Char char="T" />
+                    <Char char="I" />
+                    <Char char="C" />
+                    <Char char="A" />
+                </Phrase>
+                <Phrase margin={20} size={30} duration={1} color="#ffffff">
+                    <Char char="D" />
+                    <Char char="E" />
+                </Phrase>
+                <Phrase margin={20} size={60} duration={1} color="#ffffff">
+                    <Char char="C" />
+                    <Char char="O" />
+                    <Char char="O" />
+                    <Char char="K" />
+                    <Char char="I" />
+                    <Char char="E" />
+                    <Char char="S" />
+                </Phrase>
+            </ProductH1>
+            </ProductHeaderContainer>
+            <Footer />
         </div>
     )
 }
