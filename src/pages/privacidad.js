40c869bd7a339a0a778772bfcc8eea138fb0000c
Enrique Coscarelli <enriquecoscarelli@gmail.com>
2021-09-07 12:27:53 +0200

    Esctructura a Politica de Privacidad

--- src/pages/privacidad.js
+++ src/pages/privacidad.js
@@ -1,9 +1,44 @@
 import React from 'react'
+import Navbar from '../components/Navbar/Navbar'
+import Footer from '../components/Footer'
+import { Phrase, Char } from "animatedtxt";
+import { ProductHeaderContainer, ProductH1 } from '../components/Product/Header/ProductHeaderElements';
 
 const privacidad = () => {
     return (
         <div>
-            <h1>PRIVACIDAD</h1>
+            <Navbar />
+            <ProductHeaderContainer>
+            <ProductH1>
+                <Phrase margin={20} size={60} duration={1} color="#ffffff">
+                    <Char char="P" />
+                    <Char char="O" />
+                    <Char char="L" />
+                    <Char char="I" />
+                    <Char char="T" />
+                    <Char char="I" />
+                    <Char char="C" />
+                    <Char char="A" />
+                </Phrase>
+                <Phrase margin={20} size={30} duration={1} color="#ffffff">
+                    <Char char="D" />
+                    <Char char="E" />
+                </Phrase>
+                <Phrase margin={20} size={60} duration={1} color="#ffffff">
+                    <Char char="P" />
+                    <Char char="R" />
+                    <Char char="I" />
+                    <Char char="V" />
+                    <Char char="A" />
+                    <Char char="C" />
+                    <Char char="I" />
+                    <Char char="D" />
+                    <Char char="A" />
+                    <Char char="D" />
+                </Phrase>
+            </ProductH1>
+            </ProductHeaderContainer>
+            <Footer />
         </div>
     )
 }
