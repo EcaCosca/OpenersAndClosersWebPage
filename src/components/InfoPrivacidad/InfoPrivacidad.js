ae626b712cc63cfcfa14e247b20605f198f8b608
Enrique Coscarelli <enriquecoscarelli@gmail.com>
2021-09-09 12:05:15 +0200

    Adding information to infoPrivacidad component

--- src/components/InfoPrivacidad/InfoPrivacidad.js
+++ src/components/InfoPrivacidad/InfoPrivacidad.js
@@ -76,7 +76,57 @@ const InfoPrivacidad = () => {
                     toggle?
                         <>
                             <h2>POLÍTICA DE PRIVACIDAD - RGPD</h2>
-                            <p>Cualquier controversia o reclamo que surja que esté relacionada con las condiciones de uso, <b>Openers&Closers S.L</b> y el usuario, con renuncia expresa a cualquier otro fuero, se someten al de los juzgados y tribunales del domicilio del usuario. En el caso de que el usuario tenga su domicilio fuera de España, <b>Openers&Closers S.L</b> y el usuario se someten, con renuncia expresa a cualquier otro fuero, a los juzgados de la ciudad de Barcelona (España). Si alguna disposición de estas condiciones de uso es declarada ilegal, nula o inaplicable por un tribunal, las disposiciones restantes permanecerán en pleno vigor y efecto.</p>
+                            <p>INFORMACIÓN ADICIONAL EN MATERIA DE PROTECCIÓN DE DATOS PERSONALES</p>
+                            <p>De conformidad con lo dispuesto en el Reglamento (UE) 2016/679 de 27 de abril de 2016, de protección de las personas físicas en lo que respecta al tratamiento de sus datos personales (en adelante, el “RGPD”) y demás normativa aplicable en materia de protección de datos de carácter personal <b>OPENERS AND CLOSERS, S.L</b> trata datos relativos a clientes, usuarios y/o proveedores de acuerdo con diferentes fines como responsable del Tratamiento.</p>
+                            <p>Los datos de identificación de OPENERS AND CLOSERS son:</p>
+                            <p>OPENERS AND CLOSERS S.L.
+                            Calle Agricultura 17 (nave 12) Polígono Industrial El Pla
+                            08980 Sant Feliu de Llobregat, Barcelona
+                            Teléfono 93 408 05 15
+                            Email: info@openers-closers.com
+                            </p>
+                            <p>OPENERS & CLOSERS se compromete a proteger los datos personales de las personas con las que se relaciona o integran su organización.</p>
+                            <p><b>¿Cómo obtenemos sus datos?</b></p>
+                            <p>Los datos podrán ser recogidos a través de las siguientes fuentes:</p>
+                            <ul>
+                                <li>Al solicitar información sobre nuestras actividades, productos y/o servicios.</li>
+                                <li>Cuando se contratan productos y/o servicios tanto de forma directa como indirecta a través de la web o a través de las diferentes líneas de atención telefónica.</li>
+                                <li>Cuando se participa en campañas de marketing, a través de la web, redes sociales u otras plataformas digitales; o físicamente participando en eventos, ferias, presentaciones, en alguna promoción, premios, concursos o actividades similares.</li>
+                                <li>Cuando con autorización expresa del interesado, entidades asociadas nos comunican sus datos personales.</li>
+                                <li>Cuando una persona se suscribe a nuestra newsletter y/o boletines.</li>
+                                <li>A través del chat lo datos de contacto serán tratados con finalidad de gestionarl la información solicitada.</li>
+                                <li>A través del blog el tratamiento de la información se realizará con la finalidad de analizar el contenido de los comentarios aportados y en caso de que la persona lo solicite contestar a la petición planteada.</li>
+                            </ul>
+                            <p><b>¿Qué medidas hemos tomado para garantizar la seguridad de los datos?</b></p>
+                            <p>Desde OPENERS & CLOSERS hemos tomado, entre otras, las siguientes medidas para proteger los datos que están bajo nuestra responsabilidad.</p>
+                            <ul>
+                                <li>Hemos nombrado a un Delegado de Protección de Datos (DPO)</li>
+                                <li>Hemos adoptado unos principios de seguridad apropiados.</li>
+                                <li>Hemos evaluado los riesgos del tratamiento y el impacto que pueden causar nuestros procedimientos sobre la protección de sus datos personales.</li>
+                                <li>Hemos implantado las medidas de seguridad necesarias para preservar los datos.</li>
+                                <li>Hemos revisado las relaciones con nuestros proveedores y colaboradores externos.</li>
+                            </ul>
+                            <p><b>¿Qué tipo de datos personales son conservados y usados por O&C?</b></p>
+                            <p>Podremos recabar datos personales a través de clientes, usuarios y/o proveedores. Dichos datos personales se pueden agrupar de forma general en las categorías siguientes (en adelante “datos personales”)</p>
+                            <ul>
+                                <li>Datos identificativos y contacto, tales como el nombre, y la dirección física y electrónica.</li>
+                                <li>Datos comerciales, tales como información relativa a productos o servicios contratados; e</li>
+                                <li>Información sobre comunicaciones, tales como solicitudes de documentación, productos y/o servicio.</li>
+                            </ul>
+                            <p><b>¿Para qué finalidades conservamos y usamos los datos personales?</b></p>
+                            <p>Utilizamos los datos personales para poder gestionar la relación comercial suscrita, en la que se incluye:</p>
+                            <ul>
+                                <li>Fomentar la actividad económica</li>
+                                <li>Realizar una correcta gestión administrativa, económica y comercial.</li>
+                                <li>Atender consultas y solicitudes.</li>
+                                <li>Realizar un control de calidad sobre nuestras actividades, productos y/o servicios.</li>
+                                <li>Remitir comunicaciones personales e información y newsletters sobre nuestras actividades, productos y/o servicios que puedan ser de su interés.</li>
+                                <li>Realizar encuestas de opinión, rankings y elaborar estadísticas.</li>
+                                <li>Cumplimiento de normativas en materia de prevención de blanqueo de capitales y financiación del terrorismo</li>
+                            </ul>
+                            <p><b>¿Qué base jurídica tenemos para conservar y usar los datos personales?</b></p>
+                            <p>La base jurídica para usar los datos personales será la del desarrollo o ejecución del contrato de OPENERS & CLOSERS y el cliente. Respecto el envío de comunicaciones comerciales, la base jurídica en su caso es el interés legítimo de OPENERS & CLOSERS de acuerdo con el artículo 21.2 de la Ley 34/2002, de 11 de julio, de servicios de la sociedad de la información y de comercio electrónico, al existir una relación contractual previa, a través de la cual OPENERS & CLOSERS obtuvo de forma lícita los datos de contacto de sus clientes para emplearlos para el envío de comunicaciones comerciales referentes a productos o servicios similares a los que inicialmente fueron objeto de contratación por parte de éste.</p>
+                            <p>En determinados casos futuros podremos solicitar el consentimiento previo del cliente para usar sus datos personales. En ocasiones, podremos usar datos personales para cumplir una obligación legal impuesta a OPENERS & CLOSERS o cuando exista un interés legítimo de OPENERS & CLOSERS que justifique debidamente el uso de los datos personales para una finalidad específica, en cuyo caso le informaremos al respecto.</p>
                         </>
                         :
                         <h2>POLÍTICA DE PRIVACIDAD - RGPD</h2>
